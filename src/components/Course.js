336785e9bf3e00c8090c1db4b46006a78f18463f
Francis Quintal <francisquintal.fq@gmail.com>
2021-11-23 17:10:49 -0800

    part 2.1 completed

---
+++ src/components/Course.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header'
+import Content from './Content'
+
+const Course = ({ course }) => {
+    return (
+        <>
+            <Header name={course.name}></Header>
+            <Content parts={course.parts}></Content>
+        </>
+
+    )
+}
+export default Course
