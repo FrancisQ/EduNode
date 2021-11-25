693143f8cfdbff0a5bc6852d5c794d13576dc09f
Francis Quintal <francisquintal.fq@gmail.com>
2021-11-24 22:11:55 -0800

    part 2.6 completed using single form field to create an array of names and displaying them

--- src/App.js
+++ src/App.js
@@ -1,65 +1,43 @@
 import './App.css'
 import React, { useState } from 'react'
-import { NamedExoticComponentote } from 'react'
-// import Course from './components/Course'
 
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('What\'s you name?')
 
+  const addName = (event) => {
+    event.preventDefault()
+    console.log("button Clicked!", event.target)
+    const newPersonObject = {
+      name: newName
+    }
+    setPersons(persons.concat(newPersonObject))
+    setNewName('')
+  }
+  const updateName = (event) => {
+    setNewName(event.target.value)
+    console.log(newName)
+  }
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={updateName} />
+        </div>
+        <div>
+          <button type="submit" >add</button>
+        </div>
+      </form >
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person => <li key={person.name} >{person.name}</li>)}
+      </ul>
 
-
-// const App = () => {
-//   const courses = [
-//     {
-//       name: 'Half Stack application development',
-//       id: 1,
-//       parts: [
-//         {
-//           name: 'Fundamentals of React',
-//           exercises: 10,
-//           id: 1
-//         },
-//         {
-//           name: 'Using props to pass data',
-//           exercises: 7,
-//           id: 2
-//         },
-//         {
-//           name: 'State of a component',
-//           exercises: 14,
-//           id: 3
-//         },
-//         {
-//           name: 'Redux',
-//           exercises: 11,
-//           id: 4
-//         }
-//       ]
-//     },
-//     {
-//       name: 'Node.js',
-//       id: 2,
-//       parts: [
-//         {
-//           name: 'Routing',
-//           exercises: 3,
-//           id: 1
-//         },
-//         {
-//           name: 'Middlewares',
-//           exercises: 7,
-//           id: 2
-//         }
-//       ]
-//     }
-//   ]
-
-//   return (
-//     <>
-//       <h1> Web development curriculum</h1>
-//       {courses.map(course => {
-//         return <Course key={course.id} course={course} />
-//       })}
-//     </>
-//   )
-// }
+    </div >
+  )
+}
 
 export default App
