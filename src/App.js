37b90403dd63e56a8fa36c740524cb3e3d61360c
Francis Quintal <francisquintal.fq@gmail.com>
2021-11-27 14:08:15 -0800

    part2.9 completed - search bar feature for both name and number

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      phone: '7781231234'
-    }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('Your name?')
   const [newPhone, setNewPhone] = useState('Your number?')
+  const [newSearch, setNewSearch] = useState('Search')
+  const [newResult, setNewResult] = useState(persons)
 
+  //Submit Button onSubmit - add person object and clear fields
   const addPerson = (event) => {
     event.preventDefault()
     const nameExists = persons.find(person => person.name === newName)
-    const phoneExists = persons.find(person => person.phone === newPhone)
+    const phoneExists = persons.find(person => person.number === newPhone)
 
     if (nameExists !== undefined) {
       alert(`Name ${newName} Already Exists, try another`)
@@ -23,27 +26,39 @@ const App = () => {
     } else {
       const newPersonObject = {
         name: newName,
-        phone: newPhone
+        phone: newPhone,
+        id: persons.length + 1
       }
       setPersons(persons.concat(newPersonObject))
     }
-
     setNewName('')
     setNewPhone('')
   }
-
+  //Search input onChange
+  const searchResults = (event) => {
+    setNewSearch(event.target.value)
+    const query = event.target.value.toLowerCase()
+    let tempResults = persons.filter(person => {
+      if (person.name.toLowerCase().includes(query) || person.number.includes(query))
+        return person
+    })
+    setNewResult(tempResults)
+  }
+  //Name input onChange
   const updateName = (event) => {
     setNewName(event.target.value)
-    console.log(newName)
   }
+  //Phone input onChange
   const updatePhone = (event) => {
     setNewPhone(event.target.value)
-    console.log(newPhone)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <input value={newSearch} onChange={searchResults} />
+      <p>{searchResults}</p>
+      <h2>Add New</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={updateName} />
@@ -56,8 +71,8 @@ const App = () => {
       </form >
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => (
-          <li key={person.name} >{person.name} - {person.phone}</li>
+        {newResult.map(person => (
+          <li key={person.id} >Name: {person.name} - Phone: {person.number}</li>
         )
         )}
       </ul>
