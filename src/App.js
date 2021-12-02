70556fcface8978bdc863b442bb8326988c216b1
Francis Quintal <francisquintal.fq@gmail.com>
2021-12-02 14:57:32 -0800

    part 2.12 completed - search, conditional display, single country info display

--- src/App.js
+++ src/App.js
@@ -3,23 +3,71 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([])
+  const [results, setResults] = useState([])
+  const [search, setSearch] = useState('Search')
+  const [shortlist, setShortlist] = useState([])
 
   useEffect(() => {
     axios
-      .get('http://localhost:3001/persons')
+      .get('https://restcountries.com/v3.1/all')
       .then(response => {
-        setPersons(response.data)
+        setResults(response.data)
       })
   }, [])
 
+  const searchHandler = (event) => {
+    const query = event.target.value.toLowerCase()
+    setSearch(query)
+
+    const temp = results.filter(result => {
+      if (result.name.common.toLowerCase().includes(query))
+        return result
+    })
+
+    setShortlist(temp)
+    console.log("shortlist", shortlist)
+  }
+
   return (
-    <div>
-      {persons.map(person =>
-        <p key={person.id}>Name: {person.name} - Phone: {person.number}</p>
-      )}
+    < div class="wrapper" >
+      <input value={search} onChange={searchHandler} />
+      <Results length={shortlist.length} results={shortlist} search={search}></Results>
+
     </div >
   )
+}
+
+const Results = ({ length, results, search }) => {
+  if (search === 'Search' || search === '')
+    return <p>Use search to find country</p>
+  else if (length > 10)
+    return <p>Too many results, be more specific</p>
+  else if (length === 1) {
+    let country = results[0]
+    return (
+      < div >
+        <h1> {country.name.common}</h1>
+        <p>Capital: {country.capital}</p>
+        <p>Population: {country.population}</p>
+        <h2>Languages</h2>
+        <ul>
+          {Object.entries(country.languages).map(([key, value]) => <li key={key}>{value}</li>)}
+        </ul>
+        <img src={country.flags.png} height="100px" />
+      </div >
+    )
+
+  } else if (length > 0) {
+    return (
+      <ul>
+        {results.map(country => (
+          <li key={country.tld}>{country.name.common}</li>
+        ))}
+      </ul>
+    )
+  } else
+    return <p>Nothing Found, try again</p>
+
 }
 
 export default App
